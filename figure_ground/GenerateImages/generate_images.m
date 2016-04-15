dfd08569604dd2a1bba41c7e30586e955f74b4e0
Amy <amyjc@stanford.edu>
2016-04-15 13:59:49 -0700

    changes to figure_ground generation script

--- figure_ground/GenerateImages/generate_images.m
+++ figure_ground/GenerateImages/generate_images.m
@@ -20,19 +20,21 @@ inc=contrast*(white-gray);
 %                       Parameters and Data
 %----------------------------------------------------------------------
 % Stimulus parameters
-numLines = 150;
-angularNoise = 2*pi/20;
-maxLength = 60; % pixels
-wFigure = 100; % figure width (pixels)
-hFigure = 100; % figure height
+numLines = 120;
+angularNoise = 0;%2*pi/20;
+maxLength = 30; % pixels
+wFigure = 96; % figure width (pixels)
+hFigure = 96 ; % figure height
 
 %----------------------------------------------------------------------
 %                       Window Setup
 %----------------------------------------------------------------------
 % Open an on screen window using PsychImaging
 %[window, windowRect] = PsychImaging('OpenWindow', screenNumber, gray);
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, [], [0 0 600 600]);
-[xCenter, yCenter] = RectCenter(windowRect);
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, [], [0 0 224 224]);
+[xCenter, yCenter] = RectCenter(windowRect) 
+
+windowRect
 
 % Measure the vertical refresh rate of the monitor
 ifi = Screen('GetFlipInterval', window);
@@ -50,29 +52,41 @@ KbStrokeWait;
 w = window;
 
 
-numBackgroundTrials = 100;
-numFigureTrials = 100;
-for i = 1:numBackgroundTrials
+numBackgroundTrials = 1000;
+numFigureTrials = 1000;
+
+imageArray = zeros(numBackgroundTrials + numFigureTrials, 448, 448);
+centerCoords = zeros(numBackgroundTrials + numFigureTrials, 2);
+label = zeros([numBackgroundTrials + numFigureTrials]);
+angle = zeros([numBackgroundTrials + numFigureTrials]);
+
+for i = 1:numFigureTrials
     xCenter = wFigure/2+rand*(windowRect(3)-wFigure/2);
     yCenter = hFigure/2+rand*(windowRect(4)-hFigure/2);
     averageAngle = rand()*2*pi;
     xyGround = makeLines(round(numLines*windowRect(3)*windowRect(4)/(wFigure*hFigure)),angularNoise,averageAngle,maxLength,[0 800],[0 600]);
-    xyFigure = makeLines(numLines,angularNoise,averageAngle+rand()*(pi/2-pi/8)+pi/8,maxLength,...
+    xyFigure = makeLines(numLines,angularNoise,averageAngle+pi/2,maxLength,...
             [xCenter-wFigure/2 xCenter+wFigure/2],[yCenter-hFigure/2 yCenter+hFigure/2]);
     Screen('DrawLines', window, xyGround ,3 ,[0 0 0]);
     Screen('FillRect', window, white, [xCenter-wFigure/2 yCenter-hFigure/2 ...
             xCenter+wFigure/2 yCenter+hFigure/2], 1);
     Screen('DrawLines', window, xyFigure ,3 ,[0 0 0]);
     Screen('Flip',window);
-    imageArray = Screen('GetImage',window);
-    imwrite(imageArray, 'test.jpg')
+    imageArray(i, :, :) = mean(Screen('GetImage',window), 3);
+    centerCoords(i, 1:2) = [xCenter yCenter];
+    angle(i) = averageAngle;
+    label(i) = 1;
+    %imwrite(imageArray, 'test.jpg')
 end
 
-for i = 1:numFigureTrials
+for i = numFigureTrials:numBackgroundTrials + numFigureTrials
     averageAngle = rand()*2*pi;
     xyGround = makeLines(round(numLines*windowRect(3)*windowRect(4)/(wFigure*hFigure)),angularNoise,averageAngle,maxLength,[0 800],[0 600]);
     Screen('DrawLines', window, xyGround ,3 ,[0 0 0]);
     Screen('Flip',window);
-    imageArray = Screen('GetImage',window);
-    imwrite(imageArray, 'test.jpg')
+    imageArray(i, :, :)   = mean(Screen('GetImage',window), 3 );
+    label(i) = 0;
+    angle(i) = averageAngle; 
+    centerCoords(i, 1:2) = [xCenter yCenter];
+    %imwrite(imageArray, 'test.jpg')
 end
